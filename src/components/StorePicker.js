685b7098db05858520c9152aeb985e46570ad564
Tim Vanloocke <tim.vanloocke@roeselare.be>
2019-11-17 16:04:07 +0100

    first commit

---
+++ src/components/StorePicker.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {getFunName} from '../helpers';
+
+class StorePicker extends React.Component {
+  // constructor(){
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+  goToStore(event) {
+    event.preventDefault();
+    console.log('you changed the URL');
+    console.log(this.storeInput.value);
+    const storeId = this.storeInput.value;
+    console.log(`Going to ${storeId}`)
+    this.context.router.transitionTo(`/store/${storeId}`);
+  }
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
+        <h2> Please enter a store </h2> 
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
+        <button type="submit">Visit store -></button> 
+      </form>
+    )
+  }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
+}
+
+export default StorePicker;
